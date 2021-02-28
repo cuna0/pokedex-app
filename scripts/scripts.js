34eb2ecb76f947b1e7e8cb4cd434bceaf790130b
cuna0 <77469728+cuna0@users.noreply.github.com>
2021-02-27 17:44:51 -0800

    If applied, commitment will have added pokemonList

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+let pokemonList = [
+    {
+       name: 'Bulbasaur' ,
+       height: 0.7/m ,
+       type: ['grass' , 'poison']
+    }, 
+    { 
+       name: 'Ivysaur' ,
+       height: 1/m ,
+       type: ['grass' , 'poison']
+
+    },
+    {
+       name: 'Venusaur' ,
+       height: 2/m ,
+       type: ['grass' , 'poison']
+    }
+];
