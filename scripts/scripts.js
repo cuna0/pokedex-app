1f79147220a4124fec738651034a2fde25aa4314
cuna0 <77469728+cuna0@users.noreply.github.com>
2021-03-03 10:31:47 -0800

    if applied, commitment will have listed pokemon name and height

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,24 @@
 let pokemonList = [
     {
        name: 'Bulbasaur' ,
-       height: 0.7/m ,
+       height: 0.7 ,
        type: ['grass' , 'poison']
     }, 
     { 
        name: 'Ivysaur' ,
-       height: 1/m ,
+       height: 1 ,
        type: ['grass' , 'poison']
 
     },
     {
        name: 'Venusaur' ,
-       height: 2/m ,
+       height: 2 ,
        type: ['grass' , 'poison']
     }
-];
+]; 
+for(i = 0; i < pokemonList.length; i++) {
+   if(pokemonList[i].height < 3) {
+     document.write(pokemonList[i].name + ', ' + pokemonList[i].height)
+    }
+   
+ }
