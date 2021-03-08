a115eaad449342f2706f91e02e8b4aec90afd33a
cuna0 <77469728+cuna0@users.noreply.github.com>
2021-03-08 15:18:27 -0800

    If applied, commitment will have filled IIFE

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,8 +21,23 @@ pokemonList.forEach(function(pokemon) {
    document.write(pokemon.name + pokemon.height + pokemon.type);
  });
 
- let pokemonRepository = (function () {
-   let pokemonList = [];
+ let pokemonRepository = (function (pokemon) {
+   let pokemonList = [ {
+    name: 'Bulbasaur' ,
+    height: 0.7 ,
+    type: ['grass' , 'poison']
+ }, 
+ { 
+    name: 'Ivysaur' ,
+    height: 1 ,
+    type: ['grass' , 'poison']
+
+ },
+ {
+    name: 'Venusaur' ,
+    height: 2 ,
+    type: ['grass' , 'poison']
+ }];
  
    function add(pokemon) {
      pokemonList.push(pokemon);
